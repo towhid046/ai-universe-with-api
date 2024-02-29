147e554ebf67c3c21cb174f67007cf3267aafbe1
towhid046 <towhidmorol46@gmail.com>
2024-02-29 09:27:39 +0600

    added functionality to show more btn

--- script/main.js
+++ script/main.js
@@ -1,48 +1,40 @@
-const loadData = async () => {
+const loadData = async (isShow) => {
   const url = "https://openapi.programming-hero.com/api/ai/tools";
   const response = await fetch(url);
   const data = await response.json();
   const tools = data.data.tools;
-  displayTools(tools);
+  displayTools(tools, isShow);
 };
 
-const displayTools = (tools) => {
+const displayTools = (tools, isShow) => {
+  // primarly show 6 card
+  if (!isShow) {
+    tools = tools.slice(0, 6);
+  }
   const cardsContainer = document.getElementById("cards_container");
   tools.forEach((tool) => {
     displayEachTool(tool, cardsContainer);
   });
 };
 
-// const setFeature = (feature) => {
-//   const featureContainer = document.getElementById("features_list_container");
-//   const li = document.createElement("li");
-//   li.innerText = feature;
-//   // console.log(li)
-//   console.log(featureContainer.appendChild(li))
-// };
-
 const displayEachTool = (tool, cardsContainer) => {
   const { name, image, published_in, features, links } = tool;
 
-  
   const div = document.createElement("div");
   div.classList = `card bg-base-100 border-2`;
-  
+
   div.innerHTML = `
   <figure class="pt-7 px-7">
   <img src="${image}" class="rounded-xl"/>
   </figure>
-  
   <div class="card-body items-start">
-  
   <h2 class="text-2xl font-bold">Features</h2>
-  
  
-  <ol 
-    id="features_list_container" 
-    class="mb-5 list-decimal list-inside"
-    >
-    ${features.map(feature => `<li>${feature}</li>`).join('')}
+    <ol 
+      id="features_list_container" 
+      class="mb-5 list-decimal list-inside"
+      >
+      ${features.map((feature) => `<li>${feature}</li>`).join("")}
     </ol>
 
       <div class="w-full flex justify-between border-t-2 pt-5 items-center">
@@ -63,5 +55,12 @@ const displayEachTool = (tool, cardsContainer) => {
 
     `;
   cardsContainer.appendChild(div);
+};
+
+// show more click handelar:
+const showMoreClickHandelar = () => {
+  document.getElementById('show_more_div').classList.add('hidden')
+  isShow = true;
+  loadData(isShow);
 };
 loadData();
