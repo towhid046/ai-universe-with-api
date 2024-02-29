d7b3307c8c3be3f160c0c18091e84ecd267cc51a
towhid046 <towhidmorol46@gmail.com>
2024-02-29 08:54:59 +0600

    display all cards

--- script/main.js
+++ script/main.js
@@ -13,33 +13,45 @@ const displayTools = (tools) => {
   });
 };
 
+// const setFeature = (feature) => {
+//   const featureContainer = document.getElementById("features_list_container");
+//   const li = document.createElement("li");
+//   li.innerText = feature;
+//   // console.log(li)
+//   console.log(featureContainer.appendChild(li))
+// };
+
 const displayEachTool = (tool, cardsContainer) => {
   const { name, image, published_in, features, links } = tool;
+
+  
   const div = document.createElement("div");
   div.classList = `card bg-base-100 border-2`;
-
+  
   div.innerHTML = `
-    <figure class="pt-7 px-7">
-      <img src="${image}" class="rounded-xl"/>
-    </figure>
-
-    <div class="card-body items-start">
-
-      <h2 class="text-2xl font-bold">Features</h2>
-
-      <ol class="mb-5 list-decimal list-inside">
-        <li>Natural alnguage processing</li>
-        <li>Contextual understanding</li>
-        <li>Text generation</li>
-      </ol>
+  <figure class="pt-7 px-7">
+  <img src="${image}" class="rounded-xl"/>
+  </figure>
+  
+  <div class="card-body items-start">
+  
+  <h2 class="text-2xl font-bold">Features</h2>
+  
+ 
+  <ol 
+    id="features_list_container" 
+    class="mb-5 list-decimal list-inside"
+    >
+    ${features.map(feature => `<li>${feature}</li>`).join('')}
+    </ol>
 
       <div class="w-full flex justify-between border-t-2 pt-5 items-center">
         <div>
-          <h3 class="font-bold text-3xl">ChatGPT</h3>
+          <h3 class="font-bold text-3xl">${name}</h3>
           <span class="mr-2">
               <i class="fa-solid fa-calendar-days"></i>
           </span>
-          <span>11/01/2022</span>
+          <span>${published_in}</span>
         </div>
         
         <button class="btn border-none rounded-full bg-[#fef7f7]">
